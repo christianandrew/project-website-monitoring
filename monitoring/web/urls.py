087b49bad07b17d24e7418b003e00808fc5b5eb8
Taufan Taufik <taufanderazor@gmail.com>
2021-07-31 21:04:49 +0700

    fix dashboard dan add cabang

--- monitoring/web/urls.py
+++ monitoring/web/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('datamaster/cabang/', views.data_master_cabang, name='datamaster')
 ]
--- monitoring/web/views.py
+++ monitoring/web/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
+    #TODO: Logic buat nampilin piutang dan omsets
     return render(request, 'web/index.html')
+
+def data_master_cabang(request):
+    return render(request, 'web/datamaster_cabang.html')
