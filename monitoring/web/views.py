4363b0f4f1bca105abc9806d869e030464d13f11
Taufan Taufik <taufanderazor@gmail.com>
2021-08-22 20:28:46 +0700

    add crud cabang

--- monitoring/web/views.py
+++ monitoring/web/views.py
@@ -1,3 +1,5 @@
+from django.http.request import HttpRequest
+from .models import Cabang
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
@@ -5,8 +7,42 @@ def index(request):
     #TODO: Logic buat nampilin piutang dan omsets
     return render(request, 'web/index.html')
 
-def data_master_cabang(request):
-    return render(request, 'web/datamaster_cabang.html')
+#datamaster/cabang/
+def data_master_cabang(request: HttpRequest):
+    if request.method == 'GET':
+        cabangs = Cabang.objects.all().order_by('id')
+        ctx = {
+            'cabangs': cabangs
+        }
+        return render(request, 'web/datamaster_cabang.html', ctx)
+    else:
+        d = request.POST
+        id = d.get("id", None)
+        nama = d.get("nama", None)
+        alamat = d.get("alamat", None)
+        status = False
+        if id:
+            cabang = Cabang.objects.get(id=id)
+        if 'edit' in d:
+            cabang: Cabang = Cabang.objects.get(id=id)
+            assert nama is not None
+            cabang.nama = nama.strip()
+            assert alamat is not None
+            cabang.alamat = alamat.strip()
+            cabang.save()
+            status = True
+        elif alamat:
+            cabang: Cabang = Cabang(nama=nama, alamat=alamat)
+            cabang.save()
+            status = True
+        cabangs = Cabang.objects.all().order_by('id')    
+        ctx = {
+            'status': status,
+            'cabang': cabang,
+            'cabangs': cabangs
+        }     
+            
+        return render(request, 'web/datamaster_cabang.html', ctx)    
 
 def data_master_pengguna(request):
     return render(request, 'web/pengguna.html')
